785a926fa1e651afbbee95808f668352d878262d
Daniel <danielzember1337@gmail.com>
2020-12-21 20:31:17 +0100

    The answers are no longer random. Decide by 50-50 chance if it's going to answer positive or negative the whole survey.

--- main.py
+++ main.py
@@ -3,9 +3,26 @@ from selenium.webdriver.common.action_chains import ActionChains
 from random import randint
 import time
 
+def click_send():
+    button2 = driver.find_elements_by_class_name('btn')[1]
+    ActionChains(driver).move_to_element(button2).click(button2).perform()
 
+def positive():
+    rand = randint(0, 2)
+    return rand
+
+def negative():
+    try:
+        rand = randint(3,5)
+        return rand
+    except:
+        rand = randint(2,3)
+        return rand
+    
+    
 PATH = 'C:\Program Files (x86)\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
+
 while True:
     driver.get('https://www.survio.com/survey/d/X7W3M9I5F1Z3H7A9H')
 
@@ -18,20 +35,32 @@ while True:
 
     i = 1
 
+
+    negative_or_positive = randint(1, 2)
+    if negative_or_positive == 1:
+        print('positive answers')
+    elif negative_or_positive == 2:
+        print('negative answers')
+
+
     # kazdy blok osobitne od 1 po 8
     for answer in answers:
         print('question ' +str(i))
         i += 1
         driver.implicitly_wait(1)
+        
         items = answer.find_elements_by_class_name('item')
 
-        rand = randint(0,len(items))
-        button = items[(rand-1)]
+        if negative_or_positive == 1:
+            button = items[positive()]
+            ActionChains(driver).move_to_element(button).click(button).perform()
+            print(button.text)
+        elif negative_or_positive == 2:
+            button = items[negative()]
+            ActionChains(driver).move_to_element(button).click(button).perform()
+            print(button.text)
 
-        ActionChains(driver).move_to_element(button).click(button).perform()
-        print(button.text)
 
-    button2 = driver.find_elements_by_class_name('btn')[1]
-    ActionChains(driver).move_to_element(button2).click(button2).perform()
+    click_send()
     time.sleep(2)
     driver.refresh()
